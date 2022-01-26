9e018f6c35620a9bc8301ed20873d8ae045c1212
squach28 <squach3443@gmail.com>
2022-01-26 09:35:25 -0800

    Initial Commit

---
+++ Study-Sesh/Study_SeshApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_SeshApp.swift
+//  Study-Sesh
+//
+//  Created by Sean Quach on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Study_SeshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
