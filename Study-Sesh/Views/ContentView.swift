1a46d6eb44ce3508a817cbb9e74261aaa2c82597
squach28 <squach3443@gmail.com>
2022-01-28 14:58:40 -0800

    documentation for ContentView and StorageManager

--- Study-Sesh/Views/ContentView.swift
+++ Study-Sesh/Views/ContentView.swift
@@ -13,18 +13,19 @@ import AVKit
 
 struct ContentView: View {
     
-    @State var observer: NSKeyValueObservation?
-    @State var isPlaying: Bool = false
+    @State var observer: NSKeyValueObservation? // observes when a song is ready to play and allows the duration to be read
+    @State var isPlaying: Bool = false // boolean to check if music is playing
     @StateObject var storageManager = StorageManager()
-    @State var queuePlayer = AVQueuePlayer()
-    @State var videoPlayer = AVPlayer()
-    @State var currentItemDuration: CMTime = CMTime(seconds: 0, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
-    @State var timeObserverToken: Any?
-    @State var currentTime: CMTime = CMTime(seconds: 0, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
-    @State var imageIndex: Int = 0
-    @State var songIndex: Int = 0
+    @State var queuePlayer = AVQueuePlayer() // takes the AVPlayer and allows it to create a queue
+    @State var videoPlayer = AVPlayer() // AVPlayer that plays the audio files
+    @State var currentTime: CMTime = CMTime(seconds: 0, preferredTimescale: CMTimeScale(NSEC_PER_SEC)) // the time of the current song playing
+    @State var currentItemDuration: CMTime = CMTime(seconds: 0, preferredTimescale: CMTimeScale(NSEC_PER_SEC)) // represents the duration of the current song
+    @State var timeObserverToken: Any? // keeps track of the time when a song is playing
+    @State var imageIndex: Int = 0 // stores index of the image in StorageManager
+    @State var songIndex: Int = 0 // stores index of the song in StorageManager
     
     init() {
+        // allows audio to be played in the background if app is closed
         let audioSession = AVAudioSession.sharedInstance()
         do {
             try audioSession.setCategory(AVAudioSession.Category.playback)
@@ -46,6 +47,8 @@ struct ContentView: View {
                     .padding()
                     .animation(.easeIn, value: 1.0)
                 VStack(spacing: -10) {
+                    // Audio Progress Bar with current time and duration
+                    // TODO: make Audio Progress Bar and current time/duration into a component that takes in a parameter: AVQueuePlayer
                     ZStack(alignment: .leading) {
                         Rectangle()
                             .foregroundColor(Color(hue: 0.397, saturation: 0.609, brightness: 0.888))
@@ -60,17 +63,21 @@ struct ContentView: View {
                     }
                     .cornerRadius(45.0)
                     .padding()
+                    // End of Audio Progress Bar
                     
+                    // Current time and duration
                     HStack {
                         Text(currentItemDuration.seconds != 0 ? "\( Int(currentTime.seconds / 60)):\(String(format: "%02d", Int(currentTime.seconds.truncatingRemainder(dividingBy: 60))))" : "-:--")
                         Spacer()
                         Text(currentItemDuration.seconds != 0 ? "\( Int(currentItemDuration.seconds / 60)):\(String(format: "%02d", Int(currentItemDuration.seconds.truncatingRemainder(dividingBy: 60))))" : "-:--")
                     }
                     .padding()
+                    
+                    // End of current time and duration
                 }
                 
+                // Audio player controls
                 HStack(spacing: 50) {
-                    
                     Button(action: {
                         if songIndex - 1 < 0 {
                             let beginning = CMTime(value: 0, timescale: 1)
@@ -80,7 +87,7 @@ struct ContentView: View {
                             if imageIndex - 1 >= 0 {
                                 imageIndex -= 1
                             }
-                           
+                            
                         }
                     }, label: {
                         Image(systemName: "backward.fill")
@@ -136,6 +143,7 @@ struct ContentView: View {
                     
                 }
                 .padding(.bottom)
+                // End of Audio Player controls
                 
                 
             }
@@ -143,6 +151,36 @@ struct ContentView: View {
         
     }
     
+    // Adds an observer to the timer
+    // Stores the time in currentTime
+    func addPeriodicTimeObserver(player: AVPlayer) {
+        let timeScale = CMTimeScale(NSEC_PER_SEC)
+        let time = CMTime(seconds: 1, preferredTimescale: timeScale) // check every second for changes
+        timeObserverToken = player.addPeriodicTimeObserver(forInterval: time, queue: . main) { time in
+            self.currentTime = time
+            
+        }
+    }
+    
+    // Skip to the previous song if it isn't the first song
+    func skipToPreviousSong() {
+        if songIndex - 1 >= 0 {
+            songIndex -= 1
+            let songURL = storageManager.songs[songIndex]
+            let previousSong = AVPlayerItem(url: URL(string: songURL)!)
+            queuePlayer.replaceCurrentItem(with : previousSong)
+            self.observer = queuePlayer.currentItem?.observe(\AVPlayerItem.status) { item, _ in
+                guard let item = queuePlayer.currentItem else { return }
+                if item.status == .readyToPlay {
+                    currentItemDuration = queuePlayer.currentItem!.duration
+                }
+            }
+        }
+    }
+    
+    // Plays the current song
+    // TODO: change method function from onComplete() to play()
+    // TODO: remove adding songs to queue; handled by tracking songIndex
     func onComplete() {
         addPeriodicTimeObserver(player: queuePlayer)
         
@@ -163,31 +201,7 @@ struct ContentView: View {
         
     }
     
-    func addPeriodicTimeObserver(player: AVPlayer) {
-        let timeScale = CMTimeScale(NSEC_PER_SEC)
-        let time = CMTime(seconds: 1, preferredTimescale: timeScale)
-        timeObserverToken = player.addPeriodicTimeObserver(forInterval: time, queue: . main) { time in
-            self.currentTime = time
-            print(abs(min((325 / currentItemDuration.seconds) * currentTime.seconds, 325)))
-            
-        }
-    }
-    
-    func skipToPreviousSong() {
-        if songIndex - 1 >= 0 {
-            songIndex -= 1
-            let songURL = storageManager.songs[songIndex]
-            let previousSong = AVPlayerItem(url: URL(string: songURL)!)
-            queuePlayer.replaceCurrentItem(with : previousSong)
-            self.observer = queuePlayer.currentItem?.observe(\AVPlayerItem.status) { item, _ in
-                guard let item = queuePlayer.currentItem else { return }
-                if item.status == .readyToPlay {
-                    currentItemDuration = queuePlayer.currentItem!.duration
-                }
-            }
-        }
-    }
-    
+    // Skips to the next song if it isn't the last song
     func skipToNextSong() {
         if songIndex + 1 < storageManager.songs.count {
             songIndex += 1
@@ -201,11 +215,7 @@ struct ContentView: View {
                 }
             }
         }
-
-    }
-    
-    func getProgressBarWidth(seconds: Int) -> Int {
-        return  325 / seconds
+        
     }
 }
 
